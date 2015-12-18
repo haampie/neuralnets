0c5509aa00003a85e7034b6e964dea6372e0cdee
Harmen <harmenstoppels@gmail.com>
2015-12-18 11:41:42 +0100

    Add minover-vs-rosenblatt graph

--- assignment-2/bonusAssignmentNoisyData.m
+++ assignment-2/bonusAssignmentNoisyData.m
@@ -1,3 +1,4 @@
+%% See the history of Minover with random data.
 N = 2;
 P = 100;
 teacher = ones(N, 1);
@@ -22,3 +23,39 @@ plotline(w1, range, 'r', fighandle);
 % plotline(w2, range, 'g', fighandle);
 plotline(teacher, range, 'm', fighandle);
 plotdata(data, labels, fighandle);
+
+%% Compare Minover with Rosenblatt on random data.
+N = 100;
+alpha = 0.1:0.1:5;
+alphaValues = size(alpha, 2);
+nmax = 200;
+nD = 30;
+teacher = ones(N, 1);
+generror = zeros(alphaValues, 2);
+tol = 0.0001;
+
+for i = 1:alphaValues
+    
+    P = round(alpha(i) * N);
+    
+    learningcurves = zeros(nD, 1);
+    for j = 1:nD
+        [data, labels] = generateNoisyData(N, P, teacher, 0.3);
+        
+        w1 = minover(data, labels, nmax, tol);
+        w2 = rosenblatt(data, labels, nmax);
+        
+        % Compute learning curve
+        learningcurves(j, 1) = learningRate(w1, teacher);
+        learningcurves(j, 2) = learningRate(w2, teacher);
+    end
+    generror(i, 1) = mean(learningcurves(:, 1));
+    generror(i, 2) = mean(learningcurves(:, 2));
+end
+
+figure
+hold on;
+plot(generror(:, 1), 'b')
+plot(generror(:, 2), 'r')
+
+legend('Minover', 'Rosenblatt');
