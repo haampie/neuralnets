1abbbf65040cb8e07bbb2f6c12736217e3c7e1a1
henkvanwaarde <henkvanwaarde@gmail.com>
2015-12-28 14:15:28 +0100

    error function

--- assignment-3/gradientDescent.m
+++ assignment-3/gradientDescent.m
@@ -16,26 +16,5 @@ for i = 0:(P*tmax - 1)
     sigma = tan1 + tan2;
     w1 = w1 - eta * (sigma - tau(j))*(1 - tan1^2) * xi(:, j);
     w2 = w2 - eta * (sigma - tau(j))*(1 - tan2^2) * xi(:, j);
+    [E,Etest] = errorCalculation(w1,w2,P,xi,tau);
 end
-
-E = 0;
-
-for i = 1:P
-    tan1 = tanh(w1' * xi(:,i));
-    tan2 = tanh(w2' * xi(:,i));
-    sigma = tan1 + tan2;
-    E = E + (sigma - tau(i))^2;
-end
-
-E = E / (2 * P)
-
-Etest = 0;
-
-for i = (P+1):size(xi, 2)
-    tan1 = tanh(w1' * xi(:,i));
-    tan2 = tanh(w2' * xi(:,i));
-    sigma = tan1 + tan2;
-    Etest = Etest + (sigma - tau(i))^2;
-end
-
-Etest = Etest / (2 * (size(xi, 2) - P))
