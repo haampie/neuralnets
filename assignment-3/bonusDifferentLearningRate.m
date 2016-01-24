1fa72c95f394935d97bec99b18e6b77f72bd930c
Harmen Stoppels <harmenstoppels@gmail.com>
2016-01-24 18:07:52 +0100

    Neem een grotere tmax en een breder spectrum
    eta-waarden

--- assignment-3/bonusDifferentLearningRate.m
+++ assignment-3/bonusDifferentLearningRate.m
@@ -8,24 +8,28 @@ M = 5000;
 P = 1000;
 
 % Different sample sizes
-etaValues = 0.00001 * 2.^linspace(0, 14, 80);
+etaValues = 0.00001 * 2.^linspace(0, 20, 40);
 
 % Iterate tmax times over the training set
-tmax = 100;
+tmax = 1000;
 
 % Permute input
 permutation = randperm(M);
 permutedData = xi(:, permutation);
 permutedLabels = tau(permutation);
+initialW = normc(rand(N, 2));
+
+etaValues
 
 % Start learning
 errors = [];
 for eta = etaValues
-    [W] = trainNetwork(permutedData, permutedLabels, P, tmax, eta);
+    [W] = trainNetwork(permutedData, permutedLabels, initialW, P, tmax, eta);
     
     setError = calculateError(W, permutedData(:, P+1:end), permutedLabels(P+1:end));
     trainingError = calculateError(W, permutedData(:, 1:P), permutedLabels(1:P));
     errors = [errors; setError, trainingError];
+    fprintf('Finished for eta = %f. train = %f. test = %f\n', eta, trainingError, setError);
 end
 
 figure
