3bacf7f1a59252d2065002b11ca330f36148e9cc
Harmen Stoppels <harmenstoppels@gmail.com>
2016-01-24 18:07:26 +0100

    Gebruik dezelde initiële W tijdens de test

--- assignment-3/trainNetwork.m
+++ assignment-3/trainNetwork.m
@@ -1,7 +1,6 @@
-function [W, trainingError, setError] = trainNetwork(xi, tau, trainingSize, tmax, eta)
+function [W, trainingError, setError] = trainNetwork(xi, tau, W, trainingSize, tmax, eta)
     N = size(xi, 1);
     M = size(xi, 2);
-    W = normc(rand(N, 2));
     P = trainingSize;
     
     if nargout == 3
