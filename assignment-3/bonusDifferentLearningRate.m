7f9b5e43b4f78e0b40d3bbb6a0e6dc309ac103b5
Harmen Stoppels <harmenstoppels@gmail.com>
2016-01-24 20:02:01 +0100

    Use the relative error instead of absolute error

--- assignment-3/bonusDifferentLearningRate.m
+++ assignment-3/bonusDifferentLearningRate.m
@@ -8,7 +8,7 @@ M = 5000;
 P = 1000;
 
 % Different sample sizes
-etaValues = 0.00001 * 2.^linspace(0, 20, 40);
+etaValues = 0.00001 * 2.^linspace(0, 25, 30);
 
 % Iterate tmax times over the training set
 tmax = 1000;
@@ -32,8 +32,12 @@ for eta = etaValues
     fprintf('Finished for eta = %f. train = %f. test = %f\n', eta, trainingError, setError);
 end
 
+% Relative error
+relativeTrainingError = errors(:, 2) / calculateError(initialW, permutedData(:, 1:P), permutedLabels(1:P));
+relativeTestError = errors(:, 1) / calculateError(initialW, permutedData(:, P+1:end), permutedLabels(P+1:end));
+
 figure
+loglog(etaValues, relativeTrainingError, 'b');
 hold on;
-plot(etaValues, errors(:, 1), 'b')
-plot(etaValues, errors(:, 2), 'r')
+loglog(etaValues, relativeTestError, 'r');
 hold off;
